66c7181cfee603c237972a730bf3e5ca5473f942
TiborGalko <tibor.galko@gmail.com>
2018-11-26 19:17:49 +0100

    Entitne triedy na zadanie 5

--- src/main/java/upb/upb2018/z4/Osoba.java
+++ src/main/java/upb/upb2018/z4/Osoba.java
@@ -6,6 +6,7 @@
 package upb.upb2018.z4;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 import javax.persistence.Basic;
 import javax.persistence.Column;
@@ -13,9 +14,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import upb.upb2018.z5.Komentar;
+import upb.upb2018.z5.Subor;
 
 /**
  *
@@ -35,12 +40,24 @@ public class Osoba implements Serializable {
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;    
+    
     @Column(name = "LOGIN")
     private String login;
+    
     @Column(name = "PASSWORD")
-    private String password;    
+    private String password;
+    
     @Column(name = "SALT")
     private long salt;
+    
+    @OneToMany(mappedBy = "autor")
+    private List<Subor> subory;
+     
+    @ManyToMany(mappedBy = "zdielajuci")
+    private List<Subor> zdielaneSubory;
+    
+    @OneToMany(mappedBy = "autor")
+    private List<Komentar> komentare;
     
     public Osoba() {        
     }    
@@ -48,7 +65,7 @@ public class Osoba implements Serializable {
         this.login = login;
         this.password = password;
         this.salt = salt;
-    }
+    }  
 
     public Long getId() {
         return id;
@@ -82,13 +99,40 @@ public class Osoba implements Serializable {
         this.salt = salt;
     }
 
+    public List<Subor> getSubory() {
+        return subory;
+    }
+
+    public void setSubory(List<Subor> subory) {
+        this.subory = subory;
+    }
+
+    public List<Subor> getZdielaneSubory() {
+        return zdielaneSubory;
+    }
+
+    public void setZdielaneSubory(List<Subor> zdielaneSubory) {
+        this.zdielaneSubory = zdielaneSubory;
+    }   
+        
+    public List<Komentar> getKomentare() {
+        return komentare;
+    }
+
+    public void setKomentare(List<Komentar> komentare) {
+        this.komentare = komentare;
+    } 
+
     @Override
     public int hashCode() {
-        int hash = 5;
-        hash = 29 * hash + Objects.hashCode(this.id);
-        hash = 29 * hash + Objects.hashCode(this.login);
-        hash = 29 * hash + Objects.hashCode(this.password);
-        hash = 29 * hash + (int) (this.salt ^ (this.salt >>> 32));
+        int hash = 3;
+        hash = 11 * hash + Objects.hashCode(this.id);
+        hash = 11 * hash + Objects.hashCode(this.login);
+        hash = 11 * hash + Objects.hashCode(this.password);
+        hash = 11 * hash + (int) (this.salt ^ (this.salt >>> 32));
+        hash = 11 * hash + Objects.hashCode(this.subory);
+        hash = 11 * hash + Objects.hashCode(this.zdielaneSubory);
+        hash = 11 * hash + Objects.hashCode(this.komentare);
         return hash;
     }
 
@@ -113,11 +157,21 @@ public class Osoba implements Serializable {
         if (!Objects.equals(this.password, other.password)) {
             return false;
         }
-        return Objects.equals(this.id, other.id);
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.subory, other.subory)) {
+            return false;
+        }
+        if (!Objects.equals(this.zdielaneSubory, other.zdielaneSubory)) {
+            return false;
+        }
+        return Objects.equals(this.komentare, other.komentare);
     }
 
     @Override
     public String toString() {
-        return "Osoba{" + "id=" + id + ", login=" + login + ", password=" + password + ", salt=" + salt + '}';
-    }         
+        return "Osoba{" + "id=" + id + ", login=" + login + ", password=" + password + ", salt=" + salt + ", subory=" + subory + ", zdielaneSubory=" + zdielaneSubory + ", komentare=" + komentare + '}';
+    }    
+  
 }
